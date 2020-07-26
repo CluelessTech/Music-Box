fdc8c686c86a36b06d49ede9dcd180f160f750ca
Priyank Gandhi <gpriyank42@gmail.com>
2020-07-26 17:27:58 +0530

    Music Box

---
+++ app/src/main/java/com/priyankgandhi/mymusicplayer/MainActivity.java
@@ -0,0 +1,170 @@
+package com.priyankgandhi.mymusicplayer;
+
+import android.icu.text.SimpleDateFormat;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private MediaPlayer mediaPlayer;
+    private ImageView artistImage;
+    private TextView leftTime, rightTime;
+    private SeekBar seekBar;
+    private Button prevButton, playButton, nextButton;
+    private Thread thread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpUI();
+
+        seekBar.setMax(mediaPlayer.getDuration());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm:ss");
+                int currentPosition = mediaPlayer.getCurrentPosition();
+                int duration = mediaPlayer.getDuration();
+
+                leftTime.setText(simpleDateFormat.format(new Date(currentPosition)));
+                rightTime.setText(simpleDateFormat.format(new Date(duration - currentPosition)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    public void setUpUI(){
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.bella_ciao_el_profesor);
+
+        artistImage = findViewById(R.id.artistImage);
+        leftTime = findViewById(R.id.leftTime);
+        rightTime = findViewById(R.id.rightTime);
+        seekBar = findViewById(R.id.songTime);
+        prevButton = findViewById(R.id.prevButton);
+        playButton = findViewById(R.id.playButton);
+        nextButton = findViewById(R.id.nextButton);
+
+        prevButton.setOnClickListener(this);
+        playButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.prevButton:
+                backMusic();
+                break;
+            case R.id.playButton:
+                if(mediaPlayer.isPlaying()){
+                    pauseMusic();
+                }else{
+                    startMusic();
+                }
+                break;
+            case R.id.nextButton:
+                nextMusic();
+                break;
+        }
+    }
+
+    public void pauseMusic(){
+        if(mediaPlayer != null){
+            mediaPlayer.pause();
+            playButton.setBackgroundResource(android.R.drawable.ic_media_play);
+        }
+    }
+
+    public void startMusic(){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+            updateThread();
+            playButton.setBackgroundResource(android.R.drawable.ic_media_pause);
+        }
+    }
+
+    public void backMusic(){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.seekTo(0);
+        }
+    }
+
+    public void nextMusic(){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.seekTo(mediaPlayer.getDuration() - 1000);
+        }
+    }
+
+    public void updateThread(){
+        thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    while(mediaPlayer != null && mediaPlayer.isPlaying()){
+                        Thread.sleep(50);
+                        runOnUiThread(new Runnable() {
+                            @RequiresApi(api = Build.VERSION_CODES.N)
+                            @Override
+                            public void run() {
+                                int newPosition = mediaPlayer.getCurrentPosition();
+                                int newMaxPosition = mediaPlayer.getDuration();
+                                seekBar.setMax(newMaxPosition);
+                                seekBar.setProgress(newPosition);
+
+                                leftTime.setText(String.valueOf(new SimpleDateFormat("mm:ss")
+                                        .format(new Date(mediaPlayer.getCurrentPosition()))));
+
+                                rightTime.setText(String.valueOf(new SimpleDateFormat("mm:ss")
+                                        .format(new Date(mediaPlayer.getDuration() - mediaPlayer.getCurrentPosition()))));
+                            }
+                        });
+                    }
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(mediaPlayer != null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        thread.interrupt();
+        thread = null;
+        super.onDestroy();
+    }
+}
